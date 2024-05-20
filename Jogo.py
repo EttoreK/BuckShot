442f96889f2e7af6d1ede79e83735e904ca5337e
Ettore <ettore.dalben@gmail.com>
2024-05-20 16:10:18 -0300

    Funcional sem IA

--- Jogo.py
+++ Jogo.py
@@ -2,26 +2,34 @@ import time
 import os
 import random
 
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system("cls" if os.name == "nt" else "clear")
 
-titulo = str("\n\n" +
-            "▀█████████▄  ███    █▄   ▄████████   ▄█   ▄█▄   ▄████████    ▄█    █▄     ▄██████▄      ███    \t\t   ▄████████  ▄██████▄  ███    █▄   ▄█        ▄████████     ███         ███       ▄████████\n"+
-            "  ███    ███ ███    ███ ███    ███  ███ ▄███▀  ███    ███   ███    ███   ███    ███ ▀█████████▄\t\t  ███    ███ ███    ███ ███    ███ ███       ███    ███ ▀█████████▄ ▀█████████▄  ███    ███\n"+
-            "  ███    ███ ███    ███ ███    █▀   ███▐██▀    ███    █▀    ███    ███   ███    ███    ▀███▀▀██\t\t  ███    ███ ███    ███ ███    ███ ███       ███    █▀     ▀███▀▀██    ▀███▀▀██  ███    █▀ \n"+
-            " ▄███▄▄▄██▀  ███    ███ ███        ▄█████▀     ███         ▄███▄▄▄▄███▄▄ ███    ███     ███   ▀\t\t ▄███▄▄▄▄██▀ ███    ███ ███    ███ ███      ▄███▄▄▄         ███   ▀     ███   ▀ ▄███▄▄▄    \n"+
-            "▀▀███▀▀▀██▄  ███    ███ ███       ▀▀█████▄   ▀███████████ ▀▀███▀▀▀▀███▀  ███    ███     ███    \t\t▀▀██████▀▀   ███    ███ ███    ███ ███      ▀███▀▀▀         ███         ███    ▀▀███▀▀▀    \n"+
-            "  ███    ██▄ ███    ███ ███    █▄   ███▐██▄           ███   ███    ███   ███    ███     ███    \t\t▀████▀███▄   ███    ███ ███    ███ ███       ███    █▄      ███         ███      ███    █▄ \n"+
-            "  ███    ███ ███    ███ ███    ███  ███ ▀███▄   ▄█    ███   ███    ███   ███    ███     ███    \t\t  ███  ▀███▄ ███    ███ ███    ███ ███▌    ▄ ███    ███     ███         ███      ███    ███\n"+
-            "▄█████████▀  ████████▀  ████████▀   ███   ▀█▀ ▄████████▀    ███    █▀     ▀██████▀     ▄████▀  \t\t  ███    ▀██  ▀██████▀  ████████▀  █████▄▄██ ██████████    ▄████▀      ▄████▀    ██████████\n\n"+
-            "\t\t\t     ▄█  ▄██████▄     ▄██████▄     ▄████████    ▄████████\t\t\t\t\t   ▄████████    ▄████████  ▄█     ▄████████\n"+
-            "\t\t\t    ▐██ ███    ███   ███    ███   ███    ███   ███    ███\t\t\t\t\t  ███    ███   ███    ███ ███    ███    ███\n"+
-            "\t\t\t    ▐██ ███    ███   ███    █▀    ███    ███   ███    ███\t\t\t\t\t  ███    █▀    ███    ███ ███▌   ███    ███\n"+
-            "\t\t\t    ▐██ ███    ███  ▄███          ███    ███  ▄███▄▄▄▄██▀\t\t\t\t\t  ███          ███    ███ ███▌  ▄███▄▄▄▄██▀\n"+
-            "\t\t\t    ▐██ ███    ███ ▀▀███ ████▄  ▀███████████ ▀▀██████▀▀  \t\t\t\t\t▀███████████ ▀███████████ ███▌ ▀▀██████▀▀  \n"+
-            "\t\t\t▄   ▐██ ███    ███   ███    ███   ███    ███ ▀████▀███▄  \t\t\t\t\t         ███   ███    ███ ███  ▀████▀███▄  \n"+
-            "\t\t\t██  ▐█▀ ███    ███   ███    ███   ███    ███   ███  ▀███▄\t\t\t\t\t   ▄█    ███   ███    ███ ███    ███  ▀███▄\n"+
-            "\t\t\t ▀███▀   ▀██████▀    ████████▀    ███    █▀    ███    ▀██\t\t\t\t\t ▄████████▀    ███    █▀  █▀     ███    ▀██" +
-            "\n\n")
+titulo = str(
+    "\n\n"
+    + "▀█████████▄  ███    █▄   ▄████████   ▄█   ▄█▄   ▄████████    ▄█    █▄     ▄██████▄      ███    \t\t   ▄████████  ▄██████▄  ███    █▄   ▄█        ▄████████     ███         ███       ▄████████\n"
+    + "  ███    ███ ███    ███ ███    ███  ███ ▄███▀  ███    ███   ███    ███   ███    ███ ▀█████████▄\t\t  ███    ███ ███    ███ ███    ███ ███       ███    ███ ▀█████████▄ ▀█████████▄  ███    ███\n"
+    + "  ███    ███ ███    ███ ███    █▀   ███▐██▀    ███    █▀    ███    ███   ███    ███    ▀███▀▀██\t\t  ███    ███ ███    ███ ███    ███ ███       ███    █▀     ▀███▀▀██    ▀███▀▀██  ███    █▀ \n"
+    + " ▄███▄▄▄██▀  ███    ███ ███        ▄█████▀     ███         ▄███▄▄▄▄███▄▄ ███    ███     ███   ▀\t\t ▄███▄▄▄▄██▀ ███    ███ ███    ███ ███      ▄███▄▄▄         ███   ▀     ███   ▀ ▄███▄▄▄    \n"
+    + "▀▀███▀▀▀██▄  ███    ███ ███       ▀▀█████▄   ▀███████████ ▀▀███▀▀▀▀███▀  ███    ███     ███    \t\t▀▀██████▀▀   ███    ███ ███    ███ ███      ▀███▀▀▀         ███         ███    ▀▀███▀▀▀    \n"
+    + "  ███    ██▄ ███    ███ ███    █▄   ███▐██▄           ███   ███    ███   ███    ███     ███    \t\t▀████▀███▄   ███    ███ ███    ███ ███       ███    █▄      ███         ███      ███    █▄ \n"
+    + "  ███    ███ ███    ███ ███    ███  ███ ▀███▄   ▄█    ███   ███    ███   ███    ███     ███    \t\t  ███  ▀███▄ ███    ███ ███    ███ ███▌    ▄ ███    ███     ███         ███      ███    ███\n"
+    + "▄█████████▀  ████████▀  ████████▀   ███   ▀█▀ ▄████████▀    ███    █▀     ▀██████▀     ▄████▀  \t\t  ███    ▀██  ▀██████▀  ████████▀  █████▄▄██ ██████████    ▄████▀      ▄████▀    ██████████\n\n"
+    + "\t\t\t     ▄█  ▄██████▄     ▄██████▄     ▄████████    ▄████████\t\t\t\t\t   ▄████████    ▄████████  ▄█     ▄████████\n"
+    + "\t\t\t    ▐██ ███    ███   ███    ███   ███    ███   ███    ███\t\t\t\t\t  ███    ███   ███    ███ ███    ███    ███\n"
+    + "\t\t\t    ▐██ ███    ███   ███    █▀    ███    ███   ███    ███\t\t\t\t\t  ███    █▀    ███    ███ ███▌   ███    ███\n"
+    + "\t\t\t    ▐██ ███    ███  ▄███          ███    ███  ▄███▄▄▄▄██▀\t\t\t\t\t  ███          ███    ███ ███▌  ▄███▄▄▄▄██▀\n"
+    + "\t\t\t    ▐██ ███    ███ ▀▀███ ████▄  ▀███████████ ▀▀██████▀▀  \t\t\t\t\t▀███████████ ▀███████████ ███▌ ▀▀██████▀▀  \n"
+    + "\t\t\t▄   ▐██ ███    ███   ███    ███   ███    ███ ▀████▀███▄  \t\t\t\t\t         ███   ███    ███ ███  ▀████▀███▄  \n"
+    + "\t\t\t██  ▐█▀ ███    ███   ███    ███   ███    ███   ███  ▀███▄\t\t\t\t\t   ▄█    ███   ███    ███ ███    ███  ▀███▄\n"
+    + "\t\t\t ▀███▀   ▀██████▀    ████████▀    ███    █▀    ███    ▀██\t\t\t\t\t ▄████████▀    ███    █▀  █▀     ███    ▀██"
+    + "\n\n"
+)
+
+dict_itens = { 0: "detector", 1: "biblia", 2: "pilula", 3: "anzol", 4: "alvo", 5: "algema", 6: "polvora"}
+
+tempo_ocioso = 0.5
+
+vez = {0: 0, 1: 0}
 
 def nivel(rodada):
     match rodada:
@@ -46,7 +54,12 @@ def nivel(rodada):
         case 6:
             vida = random.randint(4, 6)
             balas = random.randint(4, 7)
-    fase = {"vida": vida, "balas": balas, "balas-vazias": 0, "balas-perigosas": random.randint(1, balas)} 
+    fase = {
+        "vida": vida,
+        "balas": balas,
+        "balas-vazias": 0,
+        "balas-perigosas": random.randint(1, balas),
+    }
     fase["balas-vazias"] = balas - fase["balas-perigosas"]
     return fase
 
@@ -60,30 +73,35 @@ def itens(itens):
     # 7 - pólvora (duplica o dano)
     for i in range(3):
         if sum(itens.values()) == 5:
-            print("Item %s descartado, muitos itens na mesa" % random.choice(list(itens.keys())))
-        else: 
+            print(
+                "Item %s descartado, muitos itens na mesa"
+                % random.choice(list(itens.keys()))
+            )
+        else:
             itens[str(random.choice(list(itens.keys())))] += 1
-    
+
     return itens
+
 
 def engatilhar(fase):
     balas = []
-    balasVazias = fase['balas-vazias']
-    balasPerigosas = fase['balas-perigosas']
-    while len(balas) != (fase['balas-vazias'] + fase['balas-perigosas']):
-        var = random.choice(['balas-vazias', 'balas-perigosas'])
-        if var == 'balas-vazias' and balasVazias != 0:
+    balasVazias = fase["balas-vazias"]
+    balasPerigosas = fase["balas-perigosas"]
+    while len(balas) != (fase["balas-vazias"] + fase["balas-perigosas"]):
+        var = random.choice(["balas-vazias", "balas-perigosas"])
+        if var == "balas-vazias" and balasVazias != 0:
             balas.append(0)
-            balasVazias -=1
-        elif var == 'balas-perigosas' and balasPerigosas != 0:
+            balasVazias -= 1
+        elif var == "balas-perigosas" and balasPerigosas != 0:
             balas.append(1)
             balasPerigosas -= 1
-    #print("balas: ", balas)
+    # print("balas: ", balas)
     return balas
+
 
 def jogada(moeda, vidaIA, vidaJ, itensIA, itensJ, balas):
     dano = 1
-    item = 0
+    jogada_opcao = 0
     moeda2 = True
 
     if moeda:
@@ -98,17 +116,38 @@ def jogada(moeda, vidaIA, vidaJ, itensIA, itensJ, balas):
         vidaI = vidaJ
         itensM = itensIA.copy()
         itensI = itensJ.copy()
-    
-    while item != "N" and sum(itensM.values()) > 0:
+
+    while jogada_opcao != "N" and sum(itensM.values()) > 0:
 
         index = 0
-        print("\t\t\tJogador [Vidas: %i]\t\t\t\t\tComputador [Vidas: %i]\n" % (vidaM, vidaI))
-        for i in itensM.keys():
-            print("\t#%i\t\t[%i] - %s\t\t\t\t\t[%i] - %s" % (index, itensM[i], i, itensI[i], i))
-            index += 1
 
-        item = input("Usar item [S|N]: ").upper()
-        if item == 'S': 
+        ##### Mostra vidas e itens de jogador e computador
+        if moeda:
+            print(
+                "\t\t\tJogador [Vidas: %i]\t\t\t\t\tComputador [Vidas: %i]\n"
+                % (vidaM, vidaI)
+            )
+        else:
+            print(
+                "\t\t\tJogador [Vidas: %i]\t\t\t\t\tComputador [Vidas: %i]\n"
+                % (vidaI, vidaM)
+            )
+
+        for index_item, nome_item in enumerate(itensM.keys()):
+            if moeda:
+                print(
+                    "\t#%i\t\t[%i] - %s\t\t\t\t\t[%i] - %s"
+                    % (index_item, itensM[nome_item], nome_item, itensI[nome_item], nome_item)
+                )
+            else:
+                print(
+                    "\t#%i\t\t[%i] - %s\t\t\t\t\t[%i] - %s"
+                    % (index_item, itensI[nome_item], nome_item, itensM[nome_item], nome_item)
+                )
+        ###############################################
+
+        jogada_opcao = input("Usar item [S|N]: ").upper()
+        if jogada_opcao == "S":
             index = input("Digite o index do item: ")
 
             try:
@@ -119,29 +158,29 @@ def jogada(moeda, vidaIA, vidaJ, itensIA, itensJ, balas):
             if list(itensM.values())[index] != 0:
                 match index:
                     case 0:
-                        #detector
+                        # detector
                         detector = balas.index(1)
                         if detector != 0:
                             print("A %iª bala é perigosa" % detector)
-                        else: 
+                        else:
                             print("Não há mais nenhuma bala perigosa")
                     case 1:
-                        #biblia
+                        # biblia
                         if balas[-1] == 0:
                             balas[-1] = 1
                         else:
                             balas[-1] = 0
                         print("Bala no gatilho inverteu de periculosidade")
                     case 2:
-                        #pilula
+                        # pilula
                         if (fase["vida"] - vidaM) >= 3:
                             vidaM += 2
                             print("Item pilula usado, a vida aumentou em 2 pontos")
                         else:
-                            vidaM += (fase["vida"] - vidaM)
+                            vidaM += fase["vida"] - vidaM
                             print("Item pilula usado, a vida aumentou em 1 ponto")
                     case 3:
-                        #anzol
+                        # anzol
                         index = input("Digite o index do item para roubar: ")
 
                         try:
@@ -153,14 +192,14 @@ def jogada(moeda, vidaIA, vidaJ, itensIA, itensJ, balas):
                             print("Nenhum item roubado, gastou o item")
                         else:
                             print("Item %s roubado" % list(itensM)[index])
-                            itensM[list(itensM)[index]] += 1
-                            itensI[list(itensI)[index]] -= 1
+                            itensM[dict_itens[index]] += 1
+                            itensI[dict_itens[index]] -= 1
                     case 4:
                         # alvo
                         if len(balas):
                             balas.pop()
                     case 5:
-                        #algema
+                        # algema
                         moeda2 = not moeda2
                         print("item algema usado, agora a troca de vez é: ", moeda2)
                     case 6:
@@ -168,80 +207,105 @@ def jogada(moeda, vidaIA, vidaJ, itensIA, itensJ, balas):
                         if dano == 1:
                             dano = 2
                         print("item polvora usado, agora o dano da bala é: ", dano)
-                    
-                itensM[list(itensM)[index]] -= 1
+
+                itensM[dict_itens[index]] -= 1
             else:
                 print("Você não possui este item")
-        elif item != 'N' and item != 'S':
-            print('Digite \"S\" ou \"N\" para depois escolher o item')
-        
+
+        elif jogada_opcao != "N" and jogada_opcao != "S":
+            print('Digite "S" ou "N" para depois escolher o item')
+
     vidaI -= balas.pop(-1) * dano
-    dano = 1
 
     if moeda:
         vidaJ = vidaM
         vidaIA = vidaI
-        itensJ = itensM.copy()
-        itensIA = itensI.copy()
+        itensJ = itensM
+        itensIA = itensI
     else:
         vidaIA = vidaM
         vidaJ = vidaI
-        itensIA = itensM.copy()
-        itensJ = itensI.copy()
+        itensIA = itensM
+        itensJ = itensI
 
     if moeda2:
         moeda = not moeda
 
-    return(moeda, vidaIA, vidaJ, itensIA.copy(), itensJ.copy(), balas)
+    return (moeda, vidaIA, vidaJ, itensIA.copy(), itensJ.copy(), balas)
+
 
 while True:
     print(titulo)
-    jogas = input("Digite \"J\" para jogar, \"S\" para sair e \"T\" para ver o tutorial\n").upper()
+    opcao_inicial = input(
+        'Digite "J" para jogar, "S" para sair e "T" para ver o tutorial\n'
+    ).upper()
 
-    if jogas == "J":
-        rodada0 = 8
-        rodada = 0
-        itens_jogo = {"detector": 0, "biblia": 0, "pilula": 0, "anzol": 0, "alvo":0, "algema": 0, "polvora": 0}
-        time.sleep(1)
-        os.system('cls' if os.name == 'nt' else 'clear')
+    if opcao_inicial == "J":
+        time.sleep(tempo_ocioso)
+        os.system("cls" if os.name == "nt" else "clear")
+        rodada = -1
+        vidaIA = 0
+        itens_jogo = {
+            "detector": 0,
+            "biblia": 0,
+            "pilula": 0,
+            "anzol": 0,
+            "alvo": 0,
+            "algema": 0,
+            "polvora": 0
+        }
+
         while True:
-            if rodada0 != rodada:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                rodada0 = rodada
-                fase = nivel(rodada)
-                vidaIA = fase['vida']
-                vidaJ = fase['vida']
-                balas = engatilhar(fase)
-                moeda = random.randint(0, 1)
-                itensJ = itens_jogo.copy()
-                itensIA = itens_jogo.copy()
-            
-            if balas == []:
-                balas = engatilhar(fase)
-            
-            itensJ = itens(itensJ)
-            itensIA = itens(itensIA)
-            print("\n" + 38*"=" + "\n\t\tRodada: ", rodada, "\n" + 38* "=")
-            moeda, vidaIA, vidaJ, itensIA, itensJ, balas = jogada(moeda, vidaIA, vidaJ, itensIA, itensJ, balas)
-            time.sleep(1)
+            print("\n" + 38 * "=" + "\n\t\tRodada: ", rodada, "\n" + 38 * "=")
 
             if vidaIA <= 0:
+                os.system("cls" if os.name == "nt" else "clear")
                 rodada += 1
+                fase = nivel(rodada)
+                vidaIA = fase["vida"]
+                vidaJ = fase["vida"]
+                balas = engatilhar(fase)
+                moeda = random.randint(0, 1)
+                itensJ = itens(itens_jogo.copy())
+                itensIA = itens(itens_jogo.copy())
+
+            if balas == []:
+                balas = engatilhar(fase)
+
+            if 0 not in vez.values():
+                vez[0] = 0
+                vez[1] = 0
+
+            moeda, vidaIA, vidaJ, itensIA, itensJ, balas = jogada(
+                moeda, vidaIA, vidaJ, itensIA, itensJ, balas
+            )
+
             if vidaJ <= 0:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                os.system("cls" if os.name == "nt" else "clear")
                 print("perdeuuuuuu")
-                time.sleep(1)
+                time.sleep(tempo_ocioso)
                 break
+
             if rodada >= 8:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                os.system("cls" if os.name == "nt" else "clear")
                 print("Venceu, ganhou, agora acabou")
-                time.sleep(1)
+                time.sleep(tempo_ocioso)
                 break
-    elif jogas == "S":
+            
+            time.sleep(tempo_ocioso)
+
+    elif opcao_inicial == "S":
         print("Saindo...")
         break
+    elif opcao_inicial == "S":
+        os.system("cls" if os.name == "nt" else "clear")
+        print("Tutorial")
+        input("\nDigite qualquer tecla para voltar\n")
     else:
-        print("Digite \"J\" e aperte ENTER para começar o jogo\n" +
-              "Digite \"S\" e aperte ENTER para sair")
-        time.sleep(1)
-    os.system('cls' if os.name == 'nt' else 'clear')
+        print(
+            'Digite "J" e aperte ENTER para começar o jogo\n'
+            + 'Digite "S" e aperte ENTER para sair'
+        )
+        time.sleep(tempo_ocioso)
+
+    os.system("cls" if os.name == "nt" else "clear")
